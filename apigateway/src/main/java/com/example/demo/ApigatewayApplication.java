074dd4111b10715b75c325efcfc9959ceff29cf3
Riddhi <riddhisaraf8@gmail.com>
2026-04-10 09:56:49 +0530

    Added api gateway

--- apigateway/src/main/java/com/example/demo/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/demo/ApigatewayApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+@EnableDiscoveryClient
+public class ApigatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
 }
