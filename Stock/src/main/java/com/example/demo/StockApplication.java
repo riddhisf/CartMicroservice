1fed049139183ea4c9c95db327ef0a98ff7f923c
Riddhi <riddhisaraf8@gmail.com>
2026-04-10 10:05:10 +0530

    Added stock service

--- Stock/src/main/java/com/example/demo/StockApplication.java
+++ Stock/src/main/java/com/example/demo/StockApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+public class StockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(StockApplication.class, args);
 	}
 
 }
