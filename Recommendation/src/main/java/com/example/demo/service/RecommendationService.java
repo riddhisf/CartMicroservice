977e76340d9f398551e9d8da0b1ca7c5e9c4f419
Riddhi <riddhisaraf8@gmail.com>
2026-04-10 10:07:26 +0530

    Added recommendation service

---
+++ Recommendation/src/main/java/com/example/demo/service/RecommendationService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.model.Catalog;
+
+@Service
+public class RecommendationService implements IRecommendationService{
+	
+	@Autowired
+    RestTemplate rt;
+
+    public List<Catalog> getRecommendations(List<Integer> pids) {
+
+        Set<String> categories = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
+        List<Catalog> recommendations = new ArrayList<>();
+
+
+        for (Integer pid : pids) {
+            Catalog product = rt.getForObject("http://PRODUCTCATALOG/catalogs/products/" + pid, Catalog.class);
+            categories.add(product.getCategory());
+        }
+
+        for (String category : categories) {
+            Catalog[] categoryProducts = rt.getForObject("http://PRODUCTCATALOG/catalogs/products/category/" + category, Catalog[].class);
+
+            for (Catalog c : categoryProducts) {
+                if (pids.contains(c.getId())) continue;
+                if (seen.add(c.getId())) {
+                    recommendations.add(c);
+                }
+            }
+        }
+
+        return recommendations;
+    }
+}
